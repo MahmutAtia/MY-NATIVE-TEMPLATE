81cf8062e6a68c1363de184f8feab49f67b94639
MahmutAtia~ <mahmoud.atia.ead@gmail.com>
2023-05-01 21:03:55 +0300

    sql

--- src/screens/cover.jsx
+++ src/screens/cover.jsx
@@ -1,17 +1,142 @@
-import React from 'react';
-import {View,Text, StyleSheet, Button} from 'react-native';
+import { useEffect, useState } from "react";
+import { View, Text, StyleSheet, Button, TextInput } from "react-native";
 
-const Cover = ({navigation, route}) => {
-    const name = route.params?.name;
-    return (
-        <View className ="flex-1 items-center justify-center">
-            <Text> Cover Page</Text>
-            <Text> HI {name}</Text>
-            <Button title='go home' onPress={()=>navigation.navigate("home")} />
-        </View>
-    );
-}
+//sql
+import * as SQLlite from "expo-sqlite"; // use * as sqllite to work
 
-const styles = StyleSheet.create({})
+const Cover = ({ navigation, route }) => {
+  const name = route.params?.name;
+
+  // names
+  const [names, setnames] = useState([]);
+
+  // input name
+  const [currentName, setcurrentName] = useState("");
+
+  // sqllit db
+
+  const db = SQLlite.openDatabase("examp.db");
+
+  // use effect
+  useEffect(() => {
+    // create table
+    db.transaction((tx) => {
+      tx.executeSql(
+        "CREATE TABLE IF NOT EXISTS names (id INTEGER PRIMARY KEY AUTOINCREMENT , name TEXT)",
+        null,
+        (txObj, resultset) => setnames(resultset.rows._array),
+        (txObj, err) => console.log(err)
+      );
+    });
+
+    // get all records
+
+    db.transaction((tx) => {
+      tx.executeSql(
+        "SELECT * FROM names",
+        null, // parameters
+        (txObj, resultset) => setnames(resultset.rows._array),
+        (txObj, err) => console.log(err)
+      );
+    });
+  }, []);
+
+  //add name
+
+  const addName = () => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "INSERT INTO names (name) VALUES (?)",
+        [currentName],
+        (txObj, resultset) => {
+            console.log(resultset)
+          let existNames = [...names];
+          existNames.push({ id: resultset.insertId, name: currentName });
+          setnames(existNames);
+          setcurrentName("");
+        },
+        (txObj, err) => console.log(err)
+      );
+    });
+  };
+
+
+  // delete name
+
+  const deleteName = (id)=>{
+    console.log(id)
+
+    db.transaction(tx=>{
+        tx.executeSql("DELETE FROM names WHERE id = ?", [id],
+        (txObj,resultset)=>{
+            console.log(resultset)
+            if (resultset.rowsAffected > 0){
+                let existNames = [...names].filter(item=>item.id!==id);
+                setnames(existNames);
+            }
+        }, 
+        (txObj,err)=>console.log(err,txObj))
+    })
+  }
+
+
+ // update 
+
+ const updateName = (id)=>{
+    console.log(id)
+    db.transaction(tx=>{
+        tx.executeSql("UPDATE names SET name = ?    WHERE id = ?", [currentName , id],
+        (txObj,resultset)=>{
+            console.log(resultset)
+
+            // if succeded
+            if (resultset.rowsAffected >0){
+                let existNames = [...names];
+                const indexToupdate = existNames.findIndex(item => item.id === id)
+                console.log(indexToupdate)
+                existNames[indexToupdate].name = currentName;
+                console.log( existNames[indexToupdate])
+                setnames(existNames)
+
+            }
+        },
+        (txObj,err)=>console.log(err)
+        )
+    })
+ }
+
+  return (
+    <View className="flex-1 items-stretch justify-evenly">
+      <View>
+        <Text className="text-center text-2xl m-9"> sqllite test</Text>
+
+        {/* all records */}
+        {names?.map((item) => (
+        <View  className="flex flex-row justify-between m-3">
+          <Text className="text-center text-2xl " key={item.id}>{item.name}</Text>
+          <Button onPress={()=>deleteName(item.id)} title="Delete"/>
+          <Button onPress={()=>updateName(item.id)} title="Update"/>
+
+          </View>
+
+        ))}
+        <TextInput className="text-center text-xl text-red-400 font-bold m-5 "
+          onChangeText={(text) => setcurrentName(text)}
+          value={currentName}
+        />
+
+        <Button title="Add name to db" onPress={() => addName()} />
+      </View>
+
+      <View>
+        <Text> Cover Page</Text>
+        <Text> HI {name}</Text>
+        <Button title="go home" onPress={() => navigation.navigate("home")} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
 
 export default Cover;
--- src/screens/settings.jsx
+++ src/screens/settings.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
-import {View, StyleSheet,Text} from 'react-native';
+import React, { useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+// float button
+import { FAB } from "@rneui/themed";
+import { Dialog } from "@rneui/themed";
 
 const Setting = () => {
-    return (
-        <View>
-            <Text>Settings</Text>
-        </View>
-    );
-}
+  // toggle dialog
+  const [visable, setvisable] = useState(false);
+  return (
+    <View>
+      <Text>Settings</Text>
+      <FAB color="blue" title="create" onPress={() => setvisable(true)} />
 
-const styles = StyleSheet.create({})
+      <Dialog isVisible={visable} onBackdropPress={() => setvisable(!visable)}>
+        <Dialog.Title title="hey" />
+        <Text>Dialog body text. Add relevant information here.</Text>
+        <Dialog.Actions>
+          <Dialog.Button
+            title="CONFIRM"
+            onPress={() => {
+              console.log(`Confirmed`);
+              setvisable(!visable)
+            }}
+          />
+          <Dialog.Button title="CANCEL" onPress={()=> setvisable(!visable)
+} />
+        </Dialog.Actions>
+      </Dialog>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
 
 export default Setting;
